b6cdaae7248e83e748f93a7d94354abb62edf3e0
yjagger <35957964+yjagger@users.noreply.github.com>
2018-06-24 19:43:19 +0530

    Update background_subtraction.py

--- background_subtraction.py
+++ background_subtraction.py
@@ -1,25 +1,24 @@
 import cv2
 import numpy as np
 
-# Define a function to get the current frame from the webcam
+# function to get the current frame from the webcam
 def get_frame(cap, scaling_factor):
     # Read the current frame from the video capture object
     _, frame = cap.read()
 
-    # Resize the image
+    # Resizing the image
     frame = cv2.resize(frame, None, fx=scaling_factor, 
             fy=scaling_factor, interpolation=cv2.INTER_AREA)
 
     return frame
 
 if __name__=='__main__':
-    # Define the video capture object
+    # video capture object
     cap = cv2.VideoCapture(0)
 
-    # Define the background subtractor object
+    # THE BACKGROUND SUBTRACTOR OBJECT
     bg_subtractor = cv2.createBackgroundSubtractorMOG2()
-     
-    # Define the number of previous frames to use to learn. 
+      
     # This factor controls the learning rate of the algorithm. 
     # The learning rate refers to the rate at which your model 
     # will learn about the background. Higher value for 
@@ -27,11 +26,10 @@ if __name__=='__main__':
     # play with this parameter to see how it affects the output.
     history = 100
 
-    # Define the learning rate
+    # Learning rate
     learning_rate = 1.0/history
 
-    # Keep reading the frames from the webcam 
-    # until the user hits the 'Esc' key
+    # Keep reading the frames from the webcam untill I press the escape key
     while True:
         # Grab the current frame
         frame = get_frame(cap, 0.5)
@@ -46,7 +44,7 @@ if __name__=='__main__':
         cv2.imshow('Input', frame)
         cv2.imshow('Output', mask & frame)
 
-        # Check if the user hit the 'Esc' key
+        # Check if I hit the 'Esc' key
         c = cv2.waitKey(10)
         if c == 27:
             break
