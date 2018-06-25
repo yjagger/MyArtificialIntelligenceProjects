53a42d533333a2861a032ba918526e7a0b099d0f
Yuvraj Jagga <yuvraj.jagga90@gmail.com>
2018-06-25 15:08:02 +0530

    adding comments to scripts

--- scripts/eye_detector.py
+++ scripts/eye_detector.py
@@ -1,39 +1,39 @@
 import cv2
 import numpy as np
 
-# Load the Haar cascade files for face and eye
+# Loading the Haar cascade files for face and eye
 face_cascade = cv2.CascadeClassifier('haar_cascade_files/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haar_cascade_files/haarcascade_eye.xml')
 
-# Check if the face cascade file has been loaded correctly
+# Checking if the face cascade file has been loaded correctly
 if face_cascade.empty():
 	raise IOError('Unable to load the face cascade classifier xml file')
 
-# Check if the eye cascade file has been loaded correctly
+# Checking if the eye cascade file has been loaded correctly
 if eye_cascade.empty():
 	raise IOError('Unable to load the eye cascade classifier xml file')
 
-# Initialize the video capture object
+# Initializing the video capture object
 cap = cv2.VideoCapture(0)
 
-# Define the scaling factor
+# Defining the scaling factor
 ds_factor = 0.5
 
-# Iterate until the user hits the 'Esc' key
+# Iteratation, until the user hits the 'Esc' key
 while True:
-    # Capture the current frame
+    # Capturing the current frame
     _, frame = cap.read()
 
-    # Resize the frame
+    # Resizing the frame
     frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
 
-    # Convert to grayscale
+    # Converting to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Run the face detector on the grayscale image
+    # Running the face detector on the grayscale image
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # For each face that's detected, run the eye detector
+    # For each face detected, running the eye detector
     for (x,y,w,h) in faces:
         # Extract the grayscale face ROI
         roi_gray = gray[y:y+h, x:x+w]
@@ -41,10 +41,10 @@ while True:
         # Extract the color face ROI
         roi_color = frame[y:y+h, x:x+w]
 
-        # Run the eye detector on the grayscale ROI
+        # Running the eye detector on the grayscale ROI
         eyes = eye_cascade.detectMultiScale(roi_gray)
 
-        # Draw circles around the eyes
+        # Below codw will draw circles around the eyes
         for (x_eye,y_eye,w_eye,h_eye) in eyes:
             center = (int(x_eye + 0.5*w_eye), int(y_eye + 0.5*h_eye))
             radius = int(0.5 * (w_eye + h_eye))
@@ -52,16 +52,16 @@ while True:
             thickness = 3
             cv2.circle(roi_color, center, radius, color, thickness)
 
-    # Display the output
+    # Displaying the output
     cv2.imshow('Eye Detector', frame)
 
-    # Check if the user hit the 'Esc' key
+    # Checking if the user hit the 'Esc' key
     c = cv2.waitKey(1)
     if c == 27:
         break
 
-# Release the video capture object
+# Releasing the video capture object
 cap.release()
 
-# Close all the windows
+# Closing all the windows
 cv2.destroyAllWindows()
